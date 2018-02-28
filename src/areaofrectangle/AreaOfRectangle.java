235bc38a7bb406291c0fac21b6ba3b4e5cf8d718
lunor7649 <lunor7649@7HJL6Y1.Student.UGDSB.ED>
2018-02-28 11:39:25 -0500

    Commit for AreaOfRectangle

---
+++ src/areaofrectangle/AreaOfRectangle.java
@@ -0,0 +1,23 @@
+/*
+AreaOfRectangle.java
+Find the area of a 5.7 by 4.8 rectangle. Have your program display the width,
+length, and area all on separate lines.
+Created by: Lucas N-H
+Created on: February 27, 2018
+Last edited on: February 28, 2018
+*/
+package areaofrectangle;
+
+public class AreaOfRectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double h, w, a;
+        h = 5.7;
+        w = 4.8;
+        a = h*w;
+        System.out.println("Height = "+h+", Width = "+w+", Area = "+a);
+    }
+}
